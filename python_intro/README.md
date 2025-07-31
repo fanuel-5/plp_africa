c7691945f6f98caa95a8a14b36f855756051030a
yd <ze.21fanuel@gmail.com>
2025-07-31 17:09:29 +0300

    Complete Assignment 1 of Intro to Python

---
+++ python_intro/README.md
@@ -0,0 +1,2 @@
+assign_1.py - A simple program that prompts the user for two
+    numbers and an operation and displays the result of the operation.
---
+++ python_intro/assign_1.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+op = input("Enter desired operation: \n\t+ | - | * | /: ")
+
+if (op == "+"):
+    print(num1 + num2)
+elif (op == "-"):
+    print(num1 + num2)
+elif (op == "*"):
+    print(num1 * num2)
+elif (op == "/"):
+    print(num1 / num2)
+
